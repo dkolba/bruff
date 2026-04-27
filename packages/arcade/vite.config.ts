240af9e3fe7b0c3440d1802f3dcc3a193f92e897
Duncan Kolba <duncankolba@gmail.com>
2026-04-27 21:46:32 +0200

    WIP

--- packages/arcade/vite.config.ts
+++ packages/arcade/vite.config.ts
@@ -1,16 +1,22 @@
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import istanbulPlugin from "vite-plugin-istanbul";
 import { version } from "./package.json";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const packagesCwd = resolve(__dirname, "..");
+
 export default defineConfig({
+  build: { sourcemap: true },
   define: {
     __APP_VERSION__: JSON.stringify(version),
   },
   plugins: [
     istanbulPlugin({
-      exclude: ["node_modules"],
+      cwd: packagesCwd,
       extension: [".ts"],
-      include: "../game/lib/**/*",
+      include: ["game/lib/**/*", "arcade/app.ts"],
     }),
   ],
   server: {
