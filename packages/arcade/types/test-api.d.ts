ccb42592b3ecdc252882290b85c709fd454238ce
Duncan Kolba <duncankolba@gmail.com>
2026-05-17 17:58:36 +0200

    chore: cleanup

--- packages/arcade/types/test-api.d.ts
+++ packages/arcade/types/test-api.d.ts
@@ -1,17 +1,7 @@
-import type { GameState } from "../../game/lib/core/types.ts";
-import type { RenderStats } from "../../game/lib/render/render-stats.ts";
-
-type BruffE2eTestApi = Readonly<{
-  dispatchInput: (input: string) => void;
-  freezeForSnapshot: () => Promise<void>;
-  getRenderStats: () => RenderStats;
-  getState: () => GameState;
-  loadState: (state: GameState) => void;
-  stepFrames: (frameCount: number) => GameState;
-}>;
+import type { BruffTestApi } from "@bruff/game/test-api";
 
 declare global {
   interface Window {
-    __bruffTestApi?: BruffE2eTestApi;
+    __bruffTestApi?: BruffTestApi;
   }
 }
